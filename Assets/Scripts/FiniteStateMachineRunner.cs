c91812eb62125a3944f9a6ae7c6a889f76132225
Jo Sunhyeon <35026790+josunhyeon@users.noreply.github.com>
2022-08-06 08:28:37 +0900

    Update FiniteStateMachineRunner.cs

--- Assets/Scripts/FiniteStateMachineRunner.cs
+++ Assets/Scripts/FiniteStateMachineRunner.cs
@@ -5,7 +5,7 @@ public class FiniteStateMachineRunner : MonoBehaviour
 {
     public FiniteStateMachine<TState> Initialize<TState>() where TState : struct, Enum
     {
-        var finiteStateMachine = new FiniteStateMachine<TState>();
+        FiniteStateMachine<TState> finiteStateMachine = new FiniteStateMachine<TState>();
         this.finiteStateMachine = finiteStateMachine;
         return finiteStateMachine;
     }
